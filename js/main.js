03102d25b22aea9760231b016a8b92f137c9795a
Barbey Ng <barbeyyng@gmail.com>
2020-05-12 22:54:48 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     $(".content").each(function (index, element) {
         $.ajax({
             url: "https://www.instagram.com/goblue42agency/?__a=1",
@@ -9,13 +8,7 @@ $(document).ready(function () {
                 $(element).after(
                     '<a href="' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url + '"> \n ' +
                     '<div class="link-preview"> \n ' +
-                    '<div class="preview-image" style="background-image:url(' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.media_preview + ');"></div> \n ' +
-                    '<div style="width:70%;" class="link-info"> \n ' +
-                    '<h4>' + result.title + '</h4> \n ' +
-                    '<p>' + result.description + '</p> \n ' +
-                    '</div><br> \n ' +
-                    '<a href="' + result.url + '" class="url-info"><i class="far fa-link"></i>' + result.url + '</a> \n ' +
-                    '</div></a>');
+                    '<div class="preview-image" style="background-image:url(' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.media_preview + ');"></a>');
                 $(element).remove();
             }
         })
