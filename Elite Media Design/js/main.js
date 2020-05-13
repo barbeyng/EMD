bf413aee765b6e057c43e67e5c546e022865f042
Barbey Ng <barbeyyng@gmail.com>
2020-05-12 22:52:59 -0500

    Add files via upload

---
+++ Elite Media Design/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".content").each(function (index, element) {
+        $.ajax({
+            url: "https://www.instagram.com/goblue42agency/?__a=1",
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (result) {
+                $(element).after(
+                    '<a href="' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url + '"> \n ' +
+                    '<div class="link-preview"> \n ' +
+                    '<div class="preview-image" style="background-image:url(' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.media_preview + ');"></a>');
+                $(element).remove();
+            }
+        })
+    });
+});
