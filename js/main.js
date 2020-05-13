2898f41e05019d4d05548e31234cf690278ad94c
Barbey Ng <barbeyyng@gmail.com>
2020-05-12 20:45:29 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-var token = 'your access token',
-    username = 'goblue42agency', 
-    num_photos = 4;
- 
-$.ajax({ // the first ajax request returns the ID of user rudrastyh
-	url: 'https://api.instagram.com/v1/users/search',
-	dataType: 'jsonp',
-	type: 'GET',
-	data: {access_token: token, q: username}, // actually it is just the search by username
-	success: function(data){
-		console.log(data);
-		$.ajax({
-			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
-			dataType: 'jsonp',
-			type: 'GET',
-			data: {access_token: token, count: num_photos},
-			success: function(data2){
-				console.log(data2);
-				for(x in data2.data){
-					$('.media-icons').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');  
-				}
-    			},
-			error: function(data2){
-				console.log(data2);
-			}
-		});
-	},
-	error: function(data){
-		console.log(data);
-	}
+$(document).ready(function () {
+
+    $(".content a").each(function (index, element) {
+        $.ajax({
+            url: "https://www.instagram.com/goblue42agency/?__a=1",
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (result) {
+                $(element).after(
+                    '<a href="' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url + '"> \n ' +
+                    '<div class="link-preview"> \n ' +
+                    '<div class="preview-image" style="background-image:url(' + result.graphql.user.edge_owner_to_timeline_media.edges[0].node.media_preview + ');"></div> \n ' +
+                    '<div style="width:70%;" class="link-info"> \n ' +
+                    '<h4>' + result.title + '</h4> \n ' +
+                    '<p>' + result.description + '</p> \n ' +
+                    '</div><br> \n ' +
+                    '<a href="' + result.url + '" class="url-info"><i class="far fa-link"></i>' + result.url + '</a> \n ' +
+                    '</div></a>');
+                // $(element).remove();
+            }
+        })
+    });
 });
